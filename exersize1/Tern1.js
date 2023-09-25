a1f169ba5b41345c314c6a80e20a9e57abb1334b
fiskooriginal <fiskowood@gmail.com>
2023-09-25 19:38:48 +0300

    homework v1.5
    completed

--- exersize1/Tern1.js
+++ exersize1/Tern1.js
@@ -1,4 +1,4 @@
-function myfunc(a){
+function myfunc_ifElse(a){
     let result;
 
     result = a * 2;
@@ -27,6 +27,42 @@ function myfunc(a){
     }
   
 }
+
+function myfunc_switch(a){
+    let result;
+    result = a * 2;
+
+    if (a > 10) {
+        result = a;
+    }
+
+    switch (true) {
+        case (result > 5):
+            return (2 * a) + 1;
+        
+        default:
+            result = 2 * (a - 2);
+            
+            if (a < 3) {
+                result = 1;
+            }
+
+            switch (true) {
+                case (result > 4):
+                    return 5;
+                
+                default:
+                    switch (true) {
+                        case (a % 2 == 0):
+                            return 6;
+                        
+                        default:
+                            return 7;
+                    }
+            }
+    }
+}
+
 function tern1(a){
 
     return (a > 10 ? a : a * 2) > 5 ? (2 * a) + 1 : (a < 3 ? 1 : 2 * (a - 2)) > 4 
@@ -36,7 +72,8 @@ function tern1(a){
 
 let a = Math.floor(Math.random() * 100);
 console.log(`a = ${a}`);
-console.log(`my func: ${myfunc(a)}`);
+console.log(`my func (if...else): ${myfunc_ifElse(a)}`);
+console.log(`my func (switch): ${myfunc_switch(a)}`);
 console.log(`tern1: ${tern1(a)}`);
 
 
--- exersize1/Tern2.js
+++ exersize1/Tern2.js
@@ -1,7 +1,5 @@
-function myfunc(a){
+function myfunc2_ifElse(a){
   let result;
-  
-
 
   if (a > 10){
     result = 'a is bigger than 10 ';
@@ -30,6 +28,55 @@ function myfunc(a){
 
   return result;
 }
+function myfunc2_switch(a){
+  let result;
+
+  switch (true) {
+    case (a > 10):
+      result = "a is bigger than 10 ";
+      break;
+    
+    default:
+      result = "a is less than or equal to 10 ";
+  }
+
+  switch (true) {
+    case (a === 5):
+      result += "an example of a special case ";
+      break;
+    
+    default:
+      result += "";
+  }
+
+  switch (true) {
+    case (a === 15):
+      result += "but a is not 15 ";
+      break;
+    
+    default:
+      result += "";
+  }
+
+  switch (true) {
+    case (a > 5):
+      result += "and a is greater than 5 ";
+      break;
+    
+    default:
+      result += "and a is less than or equal to 5 ";
+  }
+
+  switch (true) {
+    case (a % 2):
+      result += "and a is odd ";
+      break;
+    
+    default:
+      result += "and a is even ";
+  }
+  return result;
+}
 function tern2(a){
   return (a > 10 
     ? 'a is bigger than 10 ' 
@@ -50,9 +97,9 @@ function tern2(a){
 
 let b = Math.floor(Math.random() * 20) + 1;
 console.log(`a = ${b}`);
-
-console.log(myfunc(b));
-console.log(tern2(b));
+console.log(`my func (if...else): ${myfunc2_ifElse(b)}`);
+console.log(`my func (switch): ${myfunc2_switch(b)}`);
+console.log(`tern2: ${tern2(b)}`);
 
 // условие с условным (тернарным) оператором перевести в if...else И switch()
 // результат выводить в консоль, с пощью console.log()
